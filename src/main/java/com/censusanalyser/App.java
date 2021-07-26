12e1231de6a188977998244a34b21dd964a33b62
stutisaurya <stuti.saurya@gmail.com>
2021-07-26 19:41:16 +0530

    Added:Analyse the indian state census information from csv file

--- src/main/java/com/censusanalyser/App.java
+++ src/main/java/com/censusanalyser/App.java
@@ -1,5 +1,8 @@
 package com.censusanalyser;
 
 public class App {
+	public static void main(String[] args) {
+		System.out.println("Indian Census Analyser");
+	}
 
 }
--- src/test/java/com/censusanalyser/AppTest.java
+++ src/test/java/com/censusanalyser/AppTest.java
@@ -1,5 +1,13 @@
 package com.censusanalyser;
 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
 public class AppTest {
+	@Test
+	public void shouldAnswerWithTrue() {
+		assertTrue(true);
+	}
 
 }
